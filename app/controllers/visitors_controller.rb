9edc6656fcd610e462e3afc0ca1d7c24284d196a
Jose David <josepdvahn@me.com>
2014-01-27 17:39:57 -0600

    froent-end framework

--- app/controllers/visitors_controller.rb
+++ app/controllers/visitors_controller.rb
@@ -1,8 +1,6 @@
 class VisitorsController < ApplicationController
 	def new
 		@owner = Owner.new
-		flash[:notice] = "Welcome!"
-		flash[:alert] = "My birthday is soon"
 		render 'visitors/new'
 	end
 end
